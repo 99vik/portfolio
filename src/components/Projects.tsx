18e8599be99baf80c167a6ce26c2316d96a2ae70
Viktor Miric <viktorm47@gmail.com>
2024-07-31 20:08:48 +0200

    project links with stack icons

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,9 @@
+import { RiNextjsFill, RiTailwindCssFill } from "react-icons/ri";
 import ImageSlider from "./ImageSlider";
+import { SiReactquery, SiShadcnui, SiTypescript } from "react-icons/si";
+import { BiLogoTypescript } from "react-icons/bi";
+import { BsStripe } from "react-icons/bs";
+import Image from "next/image";
 
 export default function Projects() {
   return (
@@ -17,9 +22,55 @@ export default function Projects() {
           applications. Along the way, I&apos;ve written a lot of bad code and
           gradually improved my skills. Here are some of my notable projects:
         </p>
-        <div className="my-4 h-px w-full bg-zinc-700" />
+        <div className="my-5 h-px w-full bg-zinc-700" />
         <div className="space-y-3 px-3">
-          <p className="text-xl text-emerald-500">CaseMaker</p>
+          <p className="text-center text-2xl font-semibold text-emerald-500">
+            CaseMaker
+          </p>
+          <div className="flex flex-wrap justify-center gap-3 pb-2">
+            <div className="flex w-fit items-center gap-1 rounded-full border border-zinc-800 bg-zinc-950 px-2 text-emerald-500">
+              <RiNextjsFill className="size-6" />
+              Next.js
+            </div>
+            <div className="flex w-fit items-center gap-1 rounded-full border border-zinc-800 bg-zinc-950 px-2 text-emerald-500">
+              <BiLogoTypescript className="size-6" />
+              TypeScript
+            </div>
+            <div className="flex w-fit items-center gap-1 rounded-full border border-zinc-800 bg-zinc-950 px-2 text-emerald-500">
+              <RiTailwindCssFill className="size-6" />
+              Tailwind CSS
+            </div>
+            <div className="flex w-fit items-center gap-1 rounded-full border border-zinc-800 bg-zinc-950 px-2 text-emerald-500">
+              <SiShadcnui className="size-4" />
+              Shadcn
+            </div>
+            <div className="flex w-fit items-center gap-1 rounded-full border border-zinc-800 bg-zinc-950 px-2 text-emerald-500">
+              <BsStripe className="size-4.5" />
+              Stripe
+            </div>
+            <div className="flex w-fit items-center gap-1 rounded-full border border-zinc-800 bg-zinc-950 px-2 text-emerald-500">
+              <SiReactquery className="size-5" />
+              React Query
+            </div>
+            <div className="flex w-fit items-center gap-1 rounded-full border border-zinc-800 bg-zinc-950 px-2 text-emerald-500">
+              <Image
+                src="/logos/drizzle.svg"
+                width={29}
+                height={29}
+                alt="Drizzle"
+              />
+              Drizzle
+            </div>
+            <div className="flex w-fit items-center gap-1 rounded-full border border-zinc-800 bg-zinc-950 px-2 text-emerald-500">
+              <Image
+                src="/logos/authjs.svg"
+                width={18}
+                height={18}
+                alt="Drizzle"
+              />
+              Auth.js
+            </div>
+          </div>
           <p>
             Next.js app for creating custom phone cases. Upload your own image
             and choose additional features like color, finish and case type.
@@ -37,15 +88,22 @@ export default function Projects() {
             />
           </div>
           <div className="mt-20 grid grid-cols-2 gap-6">
-            <button className="rounded-md bg-emerald-500 px-4 py-2 font-semibold transition hover:bg-emerald-600">
+            <a
+              target="_blank"
+              href="https://case-maker.vercel.app"
+              className="rounded-md bg-emerald-500 px-4 py-2 text-center font-semibold transition hover:bg-emerald-600"
+            >
               See it live
-            </button>
-            <button className="rounded-md border border-emerald-500 px-4 py-2 font-semibold text-emerald-500 transition hover:bg-zinc-800">
+            </a>
+            <a
+              target="_blank"
+              href="https://github.com/99vik/case-maker"
+              className="rounded-md border border-emerald-500 px-4 py-2 text-center font-semibold text-emerald-500 transition hover:bg-zinc-800"
+            >
               View code
-            </button>
+            </a>
           </div>
         </div>
-        <div className="my-4 h-px w-full bg-zinc-700" />
       </div>
     </section>
   );
