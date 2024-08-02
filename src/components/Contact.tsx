c275a994f6ee50ea3e5bf8061bd3901a906c2027
Viktor Miric <viktorm47@gmail.com>
2024-08-02 12:53:15 +0200

    contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,11 +1,26 @@
+import { FaGithub } from "react-icons/fa";
+
 export default function Contact() {
   return (
-    <section className="flex min-h-screen justify-center bg-zinc-900 py-20 leading-7 tracking-wide">
-      <div className="flex max-w-4xl flex-col">
+    <section className="flex justify-center bg-zinc-900 py-14 leading-7 tracking-wide">
+      <div className="flex w-full max-w-4xl flex-col">
         <div className="mb-6 flex w-full items-center justify-center gap-6">
           <div className="h-px w-20 flex-1 bg-emerald-500" />
           <h2 className="text-2xl font-bold text-emerald-500">Contact</h2>
           <div className="h-px w-20 flex-1 bg-emerald-500" />
+        </div>
+        <div className="space-y-2">
+          <p className="text-center text-sm text-zinc-400">
+            Write me an e-mail at:
+          </p>
+          <p className="text-center text-lg font-semibold text-emerald-500 md:text-2xl">
+            viktor.miric0@gmail.com
+          </p>
+        </div>
+        <div className="mt-10 flex justify-center">
+          <a href="https://github.com/99vik" target="_blank">
+            <FaGithub className="size-14 text-emerald-500 hover:text-emerald-400" />
+          </a>
         </div>
       </div>
     </section>
